ef9bec7fb92258f12dcfb4fd0070018c90d8b86b
Daniel Ramirez Mendoza <danramzdev@hotmail.com>
2020-08-09 16:57:03 -0500

    Search users functionality

--- react/github-finder/src/App.js
+++ react/github-finder/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import Search from "./components/users/Search";
 
 import axios from "axios";
 
@@ -12,26 +13,25 @@ class App extends React.Component {
     loading: false,
   };
 
-  async componentDidMount() {
-    this.setState({
-      loading: true,
-    });
+  searchUsers = async (query) => {
+    this.setState({ loading: true });
 
     const response = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${query}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
     this.setState({
-      users: response.data,
+      users: response.data.items,
       loading: false,
     });
-  }
+  };
 
   render() {
     return (
       <div className="App">
         <Navbar title="Github Finder :D" />
         <div className="container">
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
