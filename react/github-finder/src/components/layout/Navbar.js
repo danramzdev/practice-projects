376938d5a9eba7d8d9dc1c41a839db7eb1c12afe
Daniel Ramirez Mendoza <danramzdev@hotmail.com>
2020-08-09 12:51:31 -0500

    refactoring to functional components

--- react/github-finder/src/components/layout/Navbar.js
+++ react/github-finder/src/components/layout/Navbar.js
@@ -1,17 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
-export class Navbar extends Component {
-  static defaultProps = {
-    title: "Github Finder",
-  };
-
-  render() {
-    return (
-      <nav className="navbar bg-primary">
-        <h1>{this.props.title}</h1>
-      </nav>
-    );
-  }
+function Navbar(props) {
+  return (
+    <nav className="navbar bg-primary">
+      <h1>{props.title}</h1>
+    </nav>
+  );
 }
+
+Navbar.defaultProps = {
+  title: "Github Finder",
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
 export default Navbar;
