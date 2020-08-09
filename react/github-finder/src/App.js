34faa53feed258b15141ae075b20517f0f082b70
Daniel Ramirez Mendoza <danramzdev@hotmail.com>
2020-08-09 15:34:35 -0500

    request data from github api

--- react/github-finder/src/App.js
+++ react/github-finder/src/App.js
@@ -2,15 +2,35 @@ import React from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 
+import axios from "axios";
+
 import "./App.css";
 
 class App extends React.Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({
+      loading: true,
+    });
+
+    const response = await axios.get("https://api.github.com/users");
+
+    this.setState({
+      users: response.data,
+      loading: false,
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar title="Github Finder :D" />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
