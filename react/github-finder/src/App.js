b36bd63c03776f836e16e6b8557c93ab56d427ce
Daniel Ramirez Mendoza <danramzdev@hotmail.com>
2020-10-04 19:07:02 -0500

    Alert state adn component

--- react/github-finder/src/App.js
+++ react/github-finder/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
+import { Alert } from "./components/layout/Alert";
 
 import axios from "axios";
 
@@ -11,6 +12,7 @@ class App extends React.Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
   };
 
   searchUsers = async (query) => {
@@ -26,6 +28,7 @@ class App extends React.Component {
     });
   };
 
+  // Clear user from state
   clearUsers = () => {
     this.setState({
       users: [],
@@ -33,15 +36,29 @@ class App extends React.Component {
     });
   };
 
+  // Set alert
+  setAlert = (message, type) => {
+    this.setState({
+      alert: {
+        message,
+        type,
+      },
+    });
+
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  };
+
   render() {
     return (
       <div className="App">
         <Navbar title="Github Finder :D" />
         <div className="container">
+          <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={this.state.users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
