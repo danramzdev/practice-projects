f0f4df047b7794b981aff4e8149fc0d11b84f3a0
Daniel Ramirez Mendoza <danramzdev@hotmail.com>
2020-11-28 21:19:10 -0600

    Wild Fire tracker

---
+++ react/wildfire-tracker/src/components/Map.js
@@ -0,0 +1,40 @@
+import GoogleMapReact from "google-map-react";
+import LocationMarker from "./LocationMarker";
+
+const Map = ({ eventData, center, zoom }) => {
+  const markers = eventData.map((event) => {
+    if (event.categories[0].id === 8) {
+      return (
+        <LocationMarker
+          lat={event.geometries[0].coordinates[1]}
+          lng={event.geometries[0].coordinates[0]}
+          key={event.id}
+        />
+      );
+    }
+
+    return null;
+  });
+
+  return (
+    <div className="map">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyBfKwswWYt7DUirSEa97r6KYFT7wPCS0So" }}
+        defaultCenter={center}
+        defaultZoom={zoom}
+      >
+        {markers}
+      </GoogleMapReact>
+    </div>
+  );
+};
+
+Map.defaultProps = {
+  center: {
+    lat: 42.3265,
+    lng: -122.8756,
+  },
+  zoom: 6,
+};
+
+export default Map;
